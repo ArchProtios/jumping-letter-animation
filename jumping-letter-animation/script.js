175ef81c03d35ed4b439820790a9a019760e4f84
Parimal <99127122+ArchProtios@users.noreply.github.com>
2024-02-24 14:41:03 +0530

    Add files via upload

---
+++ jumping-letter-animation/script.js
@@ -0,0 +1,7 @@
+// const words = document.getElementsByTagName("span")
+// console.log(words)
+// words.forEach((item) => {
+//     item.addEventListener("click", () => {
+//         item.classList.toggle("active")
+//     })
+// })
